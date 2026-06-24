f74a6f513e5ecf4ee37a7c164b256b113bf372e7
MustafijurMohan <mustafijurmohan@gmail.com>
2026-06-24 09:35:53 +0600

    Added Frontend URL

--- server/app.js
+++ server/app.js
@@ -25,6 +25,7 @@ app.use(cors({
 	origin: [
 		"http://localhost:5173",
 		"http://localhost:3000",
+		"https://quick-cart-git-main-mustafijurmohans-projects.vercel.app"
 	],
 	credentials: true,
 	allowedHeaders:["Content-Type", "Authorization", "token"],
